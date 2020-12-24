4f5f25698d69b2f098335bb5993a54666ce69b26
Sampo Tuisku <sampo.tuisku@gmail.com>
2020-12-24 04:39:50 +0200

    Added firebase configuration for android and ios. iOS might be broken

--- lib/models/player.dart
+++ lib/models/player.dart
@@ -2,12 +2,12 @@ class Player {
   String id;
   String playerName;
   String playerAvatar;
-  int listensCount;
+  int listensCount = 0;
 
   Player(this.id, this.playerName, this.playerAvatar);
 
   void incrementListenCount() {
-    listensCount++;
+    listensCount += 1;
   }
 }
 
